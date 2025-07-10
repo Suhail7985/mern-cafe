77df2712873563be5d398820973f6aea692083b5
Suhail7985 <suhailmansoori7985@gmail.com>
2025-07-10 15:41:09 +0530

    update-mern

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const dbuser = encodeURIComponent(process.env.DBUSER)
 const dbpass = encodeURIComponent(process.env.DBPASS)
