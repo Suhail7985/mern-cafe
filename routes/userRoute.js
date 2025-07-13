a1bb4a37b82bae17e4c0c46388b2f25d863a362e
Suhail7985 <suhailmansoori7985@gmail.com>
2025-07-13 11:59:28 +0530

    updated13july

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,11 +8,13 @@ import {
   updateUser,
   deleteUser,
   showUsers,
+  updateProfile,
 } from "../controllers/userController.js";
 Router.post("/register", register);
 Router.post("/login", login);
-Router.get("/showusers", authenticate, authorize("admin"), showUsers);
-Router.patch("/:id", authenticate, authorize("admin"), updateUser);
-Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
+Router.get("/", showUsers);
+Router.patch("/:id",  updateUser);
+Router.delete("/:id", deleteUser);
 Router.get("/:id/profile", authenticate, profile);
+Router.get("/:id/profile",authenticate,updateProfile)
 export default Router;
--- server.js
+++ server.js
@@ -11,18 +11,18 @@ app.use(cors())
 const dbuser = encodeURIComponent(process.env.DBUSER)
 const dbpass = encodeURIComponent(process.env.DBPASS)
 
-// mongoose.connect("mongodb://localhost:27017/mern-cafe").then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
-
-
-
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.rcpg0eg.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+mongoose.connect("mongodb://localhost:27017/mern-cafe").then(() => {
   app.listen(8080, () => {
     console.log("Server started");
   });
 });
+
+
+
+  // mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.rcpg0eg.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+  //   app.listen(8080, () => {
+  //     console.log("Server started");
+  //   });
+  // });
 
 app.use("/api/users", userRouter);
