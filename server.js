2c285efd8ce739b20615ce88f8265746cf2fa89a
Suhail7985 <suhailmansoori7985@gmail.com>
2025-07-17 14:20:04 +0530

    updatemernbackend

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose.connect("mongodb://localhost:27017/mern-cafe").then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
+// mongoose.connect("mongodb://localhost:27017/mern-cafe").then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
+
+
+  mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.rcpg0eg.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
   });
-});
-
-
-
-  // mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.rcpg0eg.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
-  //   app.listen(8080, () => {
-  //     console.log("Server started");
-  //   });
-  // });
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
